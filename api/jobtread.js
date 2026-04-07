ce1bc492ec81a9f11e63036edfef7c4c52adf1db
IconRemodeling19 <rross@iconremodelinggroup.com>
2026-04-07 00:46:33 -0400

    Update jobtread.js

--- api/jobtread.js
+++ api/jobtread.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
             "nodes": {
               "id": {},
               "name": {},
-              "account": { "name": {} },
+              "customer": { "name": {} },
               "customFieldValues": {
                 "nodes": {
                   "customField": { "name": {} },
